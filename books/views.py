9dd28676999a9be51d3ad8cdf53bca4446b77f71
Sadrul <hossainsadrul@gmail.com>
2023-03-13 16:24:01 +0600

    django test commit 2

--- books/views.py
+++ books/views.py
@@ -1,19 +1,15 @@
 from django.shortcuts import render
-import json
+from books.models import Book
 
-jsonFile = open('books.json').read()
-
-bookList = json.loads(jsonFile)
 
 # Create your views here.
-def index(request) :
+def index(request):
+    bookList = Book.objects.all()
     context = {'books': bookList}
     return render(request, 'books/index.html', context)
 
-def show(request, id) :
-    book = {}
-    for b in bookList:
-        if b['id'] == id :
-            book = b
+
+def show(request, id):
+    book = Book.objects.get(pk=id)
     context = {'book': book}
     return render(request, 'books/show.html', context)
